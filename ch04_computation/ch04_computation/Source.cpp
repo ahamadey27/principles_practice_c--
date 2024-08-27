6654f65e05189059d7a72c23387e4ed52f49376c
ahamadey27 <hamadey@gmail.com>
2024-08-27 12:45:07 -0400

    Start of Ch.4

---
+++ ch04_computation/ch04_computation/Source.cpp
@@ -0,0 +1,39 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+
+	/*
+	//Convers inches to cm and visa visa 
+	constexpr double cm_per_inch = 2.54; //Declares a constant
+
+	double length = 1; //Set length to inches
+
+	char unit = 0;
+
+	cout << "Please enter a length followed by unit (c or i)\n";
+	cin >> length >> unit; 
+
+	if (unit == 'i')
+	{
+		cout << length << "in ==" << cm_per_inch * length << "cm\n";
+	}
+
+	else if (unit == 'c')
+	{
+		cout << length << "cm==" << length / cm_per_inch << "in\n";
+	}
+
+	else
+	{
+		cout << "Sorry don't know what that means.\n";
+	}
+
+
+	return 0;
+	*/
+	
+
+
+
+}
